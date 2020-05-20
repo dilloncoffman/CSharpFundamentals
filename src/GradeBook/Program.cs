80d83dd2c3e35f0088ca9eeee862c3a2aff6a163
Dillon Coffman <dcoffman@ceiamerica.com>
2020-05-20 11:05:34 -0400

    Added basic Gradebook project that computer the low, high, and average grades in a Gradebook; Unit tests are written in a separate tests folder that can be run with dotnet test

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Dillon's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(76.2);
+            book.AddGrade(90.2);
+            
+            var stats = book.GetStatistics();
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The average grade is {stats.Average}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+        }
+    }
+}
---
+++ test/GradeBook.Tests/BookTests.cs
@@ -0,0 +1,27 @@
+using System.Runtime.Intrinsics.X86;
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // 1. Arrange
+            var book = new Book("");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
+
+            // 2. Act
+            var result = book.GetStatistics();
+
+            // 3. Assert
+            Assert.Equal(85.6, result.Average, 1);
+            Assert.Equal(90.5, result.High,  1);
+            Assert.Equal(77.3, result.Low, 1);
+        }
+    }
+}
