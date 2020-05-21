10f88e225ac3fc6443e0599580b61f4fb3df9fbc
Dillon Coffman <dcoffman@ceiamerica.com>
2020-05-21 10:57:02 -0400

    Defined a new DiskBook class to write and read data from a file utilizing the interface and abstract Book class defined in the previous commit

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,12 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 
-namespace GradeBook
-{
+namespace GradeBook {
   public class NamedObject // Ideally would be in its own file, but for learning purposes it's okay to have inside Book.cs
   {
-    public NamedObject(string name)
-    {
+    public NamedObject (string name) {
       Name = name;
     }
 
@@ -19,98 +18,88 @@ namespace GradeBook
 
   public interface IBook // C# convention to begin interface names with "I"
   {
-    void AddGrade(double grade);
-    Statistics GetStatistics();
+    void AddGrade (double grade);
+    Statistics GetStatistics ();
     string Name { get; } // interface guarantees Name will AT LEAST have a getter
   }
 
-  public abstract class Book: NamedObject, IBook // classes that derive from this class, should have different implementations of AddGrade
+  public abstract class Book : NamedObject, IBook // classes that derive from this class, should have different implementations of AddGrade
   {
-    protected Book(string name) : base(name)
-    {  
+    protected Book (string name) : base (name) { }
+
+    public abstract void AddGrade (double grade);
+
+    public abstract Statistics GetStatistics ();
+  }
+
+  public class DiskBook : Book {
+    public DiskBook (string name) : base (name) { }
+
+    public override void AddGrade (double grade) {
+      using (var writer = File.AppendText ($"{Name}.txt")) {
+        writer.WriteLine (grade);
+      }
     }
 
-    public abstract void AddGrade(double grade);
+    public override Statistics GetStatistics () {
+      var result = new Statistics();
 
-    public virtual Statistics GetStatistics()
-    {
-      throw new NotImplementedException();
+      using(var reader = File.OpenText($"{Name}.txt"))
+      {
+        var line = reader.ReadLine();
+        while (line != null) {
+          var number = double.Parse(line);
+          result.Add(number);
+          line = reader.ReadLine();
+        }
+      }
+
+      return result;
     }
   }
 
   public class InMemoryBook : Book // Named InMemoryBook because it's AddGrade method will store the grades in memory, whereas another class that extends BookBase may choose to store the grades in a file or over a network
   {
 
-    public InMemoryBook(string name) : base(name)
-    {
+    public InMemoryBook (string name) : base (name) {
       category = ""; // readonly vars can be changed/reinitialized in constructor
       this.Name = name;
-      this.grades = new List<double>();
+      this.grades = new List<double> ();
     }
 
-    public void AddGrade(char letter)
-    {
-        switch(letter)
-        {
-          case 'A':
-            AddGrade(90);
-            break;
-          case 'B':
-            AddGrade(80);
-            break;
-          case 'C':
-            AddGrade(70);
-            break;
-          case 'D':
-            AddGrade(60);
-            break;
-          default:
-            AddGrade(0);
-            break;
-        }
-    }
-
-    public override void AddGrade(double grade)
-    {
-
-      if (grade <= 100 && grade >= 0)
-        this.grades.Add(grade);
-      else
-        throw new ArgumentException($"Invalaid {nameof(grade)}");
-    }
-
-    public override Statistics GetStatistics()
-    {
-      var result = new Statistics();
-      result.Average = 0.0;
-      result.High = double.MinValue;
-      result.Low = double.MaxValue;
-
-      foreach(var grade in grades)
-      {
-          result.Low = Math.Min(grade, result.Low);
-          result.High = Math.Max(grade, result.High);
-          result.Average += grade;
-      }
-      result.Average /= grades.Count;
-
-      switch(result.Average)
-      {
-        case var d when d >= 90.0:
-          result.Letter = 'A';
+    public void AddGrade (char letter) {
+      switch (letter) {
+        case 'A':
+          AddGrade (90);
           break;
-        case var d when d >= 80.0:
-          result.Letter = 'B';
+        case 'B':
+          AddGrade (80);
           break;
-        case var d when d >= 70.0:
-          result.Letter = 'C';
+        case 'C':
+          AddGrade (70);
           break;
-        case var d when d >= 60.0:
-          result.Letter = 'd';
+        case 'D':
+          AddGrade (60);
           break;
         default:
-          result.Letter = 'F';
+          AddGrade (0);
           break;
+      }
+    }
+
+    public override void AddGrade (double grade) {
+
+      if (grade <= 100 && grade >= 0)
+        this.grades.Add (grade);
+      else
+        throw new ArgumentException ($"Invalaid {nameof(grade)}");
+    }
+
+    public override Statistics GetStatistics () {
+      var result = new Statistics ();
+
+      foreach (var grade in grades) {
+        result.Add (grade);
       }
 
       return result;
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,55 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace GradeBook
-{
-    class Program
-    {
-        static void Main(string[] args)
-    {
-      var book = new InMemoryBook("Dillon's Grade Book");
+namespace GradeBook {
+    class Program {
+        static void Main (string[] args) {
+            var book = new DiskBook ("Dillon's Grade Book");
 
-      EnterGrades(book);
+            EnterGrades (book);
 
-      var stats = book.GetStatistics();
-      Console.WriteLine($"consts are static members on a class: {InMemoryBook.CATEGORY}");
-      Console.WriteLine($"For the book named {book.Name}");
-      Console.WriteLine($"The lowest grade is {stats.Low}");
-      Console.WriteLine($"The average grade is {stats.Average}");
-      Console.WriteLine($"The highest grade is {stats.High}");
-      Console.WriteLine($"The letter grade is {stats.Letter}");
+            var stats = book.GetStatistics ();
+            Console.WriteLine ($"consts are static members on a class: {InMemoryBook.CATEGORY}");
+            Console.WriteLine ($"For the book named {book.Name}");
+            Console.WriteLine ($"The lowest grade is {stats.Low}");
+            Console.WriteLine ($"The average grade is {stats.Average}");
+            Console.WriteLine ($"The highest grade is {stats.High}");
+            Console.WriteLine ($"The letter grade is {stats.Letter}");
+        }
+
+        private static void EnterGrades (IBook book) {
+            while (true) {
+                Console.WriteLine ("Please enter a grade. Enter 'q' when done to quit.");
+                var input = Console.ReadLine ();
+                if (input.Equals ("q")) {
+                    break;
+                }
+
+                try {
+                    var grade = double.Parse (input);
+                    book.AddGrade (grade);
+                } catch (ArgumentException ex) {
+                    Console.WriteLine (ex.Message);
+                } catch (FormatException ex) {
+                    Console.WriteLine (ex.Message);
+                } finally {
+                    Console.WriteLine ("**");
+                }
+
+            }
+        }
     }
-
-    private static void EnterGrades(IBook book)
-    {
-      while (true)
-      {
-        Console.WriteLine("Please enter a grade. Enter 'q' when done to quit.");
-        var input = Console.ReadLine();
-        if (input.Equals("q"))
-        {
-          break;
-        }
-
-        try
-        {
-          var grade = double.Parse(input);
-          book.AddGrade(grade);
-        }
-        catch (ArgumentException ex)
-        {
-          Console.WriteLine(ex.Message);
-        }
-        catch (FormatException ex)
-        {
-          Console.WriteLine(ex.Message);
-        }
-        finally
-        {
-          Console.WriteLine("**");
-        }
-
-      }
-    }
-  }
 }
